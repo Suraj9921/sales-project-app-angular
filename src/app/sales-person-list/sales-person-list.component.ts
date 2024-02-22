16b38b16d9ce27ced7c0e1985de98afdb3922584
suraj <suraj14o99@gmail.com>
2024-02-22 14:05:52 +0530

    Commit message for changes in sales-project

---
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.bootstrap.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent {
+  salesPersonList : SalesPerson[] = [
+    new SalesPerson("Suraj","S","suraj@gmail.com",50000),
+    new SalesPerson("Milan","Joseph","milan@gmail.com",60000),
+    new SalesPerson("Rahul","Vijayan","rahul@gmail.com",30000),
+    new SalesPerson("Bob","Samual","bob@gmail.com",90000)
+  ];
+}
